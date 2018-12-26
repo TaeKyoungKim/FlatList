f8e97d3521d41007b81530049b8ae0223ad494bb
Garry Kim <moduedu@gamil.com>
2018-12-26 14:39:35 +0900

    flatList initialize

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, View,StyleSheet } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isRefreshing: false,
+      data : [
+        {key: 'a', title: '테슬라'}, 
+        {key: 'b', title:'현대'}
+      ]
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex:1, paddingTop:30}}>
+        <FlatList
+          data={this.state.data}
+          renderItem={ ({item}) => <CarItem title={item.title}/>}
+          ItemSeparatorComponent={ () => (
+            <View style={{
+              height: StyleSheet.hairlineWidth, 
+              marginLeft:20, 
+              backgroundColor:'red'
+            }} />
+          )}
+          refreshing = {this.state.isRefreshing}
+          onRefresh={ ()=>{
+            this.setState({
+              isRefreshing : true
+            })
+            setTimeout(() => {
+              this.setState({
+                isRefreshing: false,
+                data : [
+                  {key: 'a', title: '테슬라'}, 
+                  {key: 'b', title:'현대'},
+                  {key: 'c', title:'BMW'}
+                ]
+              })
+            }, 5000);
+          }}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
